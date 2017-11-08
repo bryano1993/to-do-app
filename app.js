be5c13a66a99262315bce6f1808b2687e6d85147
Bryan <bryano1993@gmail.com>
2017-11-08 08:23:08 -0800

    Assignment 7 completed

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
  const toDoList = document.getElementById('toDoList');
 
- addToDoForm.addEventListener('submit', () => {
-
-    event.preventDefault();
+ addToDoForm.addEventListener('submit', (e) => {
+//prevents the page from reloading
+    e.preventDefault();
 
     //get the text
     let title = newToDoText.value
 
+    //clears out newToDoText
+    newToDoText.value = '';
+
     //create a new li
     let newLi = document.createElement('Li');
+
+    //set the title
+    newLi.textContent = title;
+
 
     //create a new input
     let checkbox = document.createElement('input');
@@ -19,17 +26,35 @@ function onReady() {
     //set the input's type to checkbox
     checkbox.type = "checkbox";
 
-    //set the title
-    newLi.textContent = title;
+    //create a new input
+    let button = document.createElement('button');
+
+    //set the input's type to checkbox
+    button.textContent = "remove";
+
 
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    //attach the button to the li
+    newLi.appendChild(button);
+
+
+    //places newli into toDoList
     toDoList.appendChild(newLi);
 
-    newToDoText.value = '';
 
   });
+
+ toDoList.addEventListener('click', (e) => {
+   if(e.target.tagName === 'BUTTON'){
+   const li = e.target.parentNode;
+   const ul = li.parentNode;
+   toDoList.removeChild(li);
+ }
+});
+
+
 }
 
 
